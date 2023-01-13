3b4ae19effb638d4ee45a0f301fda9976599603e
Joe <joep@nyu.edu>
2023-01-13 15:29:58 -0500

    Template init

---
+++ frontend/src/App.svelte
@@ -0,0 +1,9 @@
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
+
+<div class="navbar bg-violet-900">
+    <a class="btn btn-ghost normal-case text-xl text-white">Printer Status Page</a>
+</div>
